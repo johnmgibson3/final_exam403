80dd26c95dba00987d3d97b9e120063927ad3eb4
John Gibson <johnmgibson3@gmail.com>
2024-11-29 12:04:13 -0700

    new repo for 403 test

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    port: process.env.PORT,
+    db: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      name: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT, 10),
+    },
+  };
